4f4dc8ffde234e0be4a52218a02d032607de8a2d
Ryan Delgado <rdelgado@ptpos.com>
2022-12-19 13:49:15 -0700

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace MauiIssuesApp;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+
+        for (int i = 0; i < 10; i++)
+        {
+            MenuItems.Add(new TestItem { Name = $"Button {i}", Description = (i * 10).ToString() });
+        }
+        CommandRemove = new Command(() =>
+        {
+            if (LineItems.Any())
+            {
+                if (LineItems.Count > 1) LineItems.RemoveAt(1);
+                else LineItems.RemoveAt(0);
+            }
+        });
+        BindingContext = this;
+    }
+
+    public ObservableCollection<TestItem> MenuItems { get; set; } = new ObservableCollection<TestItem>();
+    public ObservableCollection<TestItem2> LineItems { get; set; } = new ObservableCollection<TestItem2>();
+
+    public ICommand CommandRemove { get; set; }
+
+    private void ListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        var mivm = (e.SelectedItem as TestItem);
+
+        LineItems.Add(new TestItem2 { Nam = mivm.Name, Desc = mivm.Description });
+    }
+}
+
+public class TestItem
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
+
+public class TestItem2
+{
+    public string Nam { get; set; }
+    public string Desc { get; set; }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiIssuesApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
